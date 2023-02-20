92f3a01b938dde1b2b27b1a3568878135df0d1d3
sotiriosc <chortogiannoss@gmail.com>
2023-02-19 21:13:28 -0500

    working on project

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Professional README Generator Starter Code
+# README Creator
+        
+        ## Motivation:
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+        To construct an application that creates README's.
+
+        ## The Purpose of This Project:
+
+        I Built this project to provide developers and easy way to create their README's. I made it easier to build README's. I learned how to utilize nodejs and javascript. To create an application that will build README's.
+        
--- index.js
+++ index.js
@@ -1,7 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", 
+    "What was your motivation?", 
+    "Why did you build this project?", 
+    "What problem does it solve?",
+    "What did you learn?",
+
+];
+
+const [one, two, three, four, five] = questions
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: one,
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: two, 
+        },
+        {
+            type: 'input',
+            name: 'build',
+            message: three,
+        },
+        {
+            type: 'input',
+            name: 'solve',
+            message: four,
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: five,
+        },
+    ])
+
+    // .then((data) => {
+    
+    //     fs.writeFile('README.md', JSON.stringify(data, null, '\t'), (err) =>
+    //       err ? console.log(err) : console.log('Success!')
+    //     );
+    //   });
+
+      .then((data) => {
+    
+        fs.writeFile('README.md', `# ${data.title}
+        
+        ## Motivation:
+
+        ${data.motivation}
+
+        ## The Purpose of This Project:
+
+        ${data.build} ${data.solve} ${data.learn}
+        `, (err) =>
+          err ? console.log(err) : console.log('Success!')
+        );
+      });
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +75,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
